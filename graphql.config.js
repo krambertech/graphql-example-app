04d53445f0aca625e010c94572e78152e7ab3415
Kateryna Porshnieva <k.porshnieva@gmail.com>
2021-03-25 09:51:14 +0200

    refactor: add comments into configuration files

--- graphql.config.js
+++ graphql.config.js
@@ -3,11 +3,18 @@ require('dotenv').config();
 
 module.exports = {
   overwrite: true,
+  /**
+   * We use local schema for generation since it speeds up the process
+   */
   schema: 'schema.graphql',
   documents: './src/**/*.graphql',
   extensions: {
     codegen: {
       generates: {
+        /**
+         * Generates typescript definitions and code for apollo hooks near
+         * the operation in __generated__ folder
+         */
         'src/': {
           config: {
             dedupeOperationSuffix: true,
@@ -28,21 +35,30 @@ module.exports = {
             folder: '__generated__',
           },
         },
+        /**
+         * All global types from schema get generated in src/__generated__/types.ts
+         * file, these types are used in operation types as well
+         */
         'src/__generated__/types.ts': {
           config: {
             namingConvention: 'keep',
           },
           plugins: ['typescript'],
         },
+        /**
+         * In order to keep local schema up-to-date with remote one, here we
+         * fetch remove schema and save it locally
+         */
         './schema.graphql': {
           schema: process.env.REACT_APP_API_URL,
           plugins: ['schema-ast'],
         },
-        // 'src/__generated__/typeDefs.ts': {
-        //   plugins: ['dev/printTypeDefs.js'],
-        // },
       },
     },
+    /**
+     * This endpoint configuration needed for VScode and other plugins to work, then you
+     * have an ability to run requests right in IDE
+     */
     endpoints: {
       default: process.env.REACT_APP_API_URL,
     },

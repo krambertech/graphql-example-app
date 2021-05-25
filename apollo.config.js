fcc0abd43a5d55fbc24da2acc2b70eb754f3134a
Kateryna Porshnieva <k.porshnieva@gmail.com>
2021-05-25 15:48:25 +0300

    chore: modify apollo ext config

--- apollo.config.js
+++ apollo.config.js
@@ -1,9 +1,8 @@
 // This config is need for Apollo VSCode extension to work
 // https://marketplace.visualstudio.com/items?itemName=apollographql.vscode-apollo
 module.exports = {
-  service: {
-    name: 'graphql-example',
-    localSchemaFile: './schema.graphql',
+  client: {
+    service: 'Countries-API-677la',
+    includes: ['src/**/*.graphql'],
   },
-  includes: ['src/**/*.graphql'],
 };
